7c5ed6227761205a79f4f9a46eaa71ef60a44c38
Isha <isha@geekyants.com>
2022-09-27 17:48:41 +0530

    input field + favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Input from './components/input'
 
 function App() {
   return (
     <Router>
-      <h1>Done with setup</h1>
+      <Input/>
     </Router>
   );
 }
