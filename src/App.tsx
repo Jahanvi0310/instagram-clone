0de5cf61cb11d227ba441806d604d24227d084b3
Pulkit Anand <pulkitanand@Pulkit-Anands-MacBook-Pro.local>
2022-09-28 18:36:28 +0530

    navbar carried forward

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from 'styled-components';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Header from './components/Header';
+const App=()=>{
+  return(
+    <Container>
+   
+<Header/>
+    </Container>
+  )
 }
-
 export default App;
+const Container=styled.div`
+
+
+
+`;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import React from 'react';
+const Home=()=>{
+    return(
+        <Conatiner>
+            <h1>home page</h1>
+        </Conatiner>    
+        )
+}
+export default Home;
+const Conatiner=styled.div`
+`;
