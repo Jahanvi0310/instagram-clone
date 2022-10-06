7763ebb94d71eb0961ab0d6137718839c905e31b
Isha <isha@geekyants.com>
2022-10-06 20:33:31 +0530

    refactor/input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Input from './components/input'
+import Input from "./components/input";
 
 function App() {
   return (
     <Router>
-      <Input/>
+      <Input />
     </Router>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,67 +1,44 @@
-// import React from 'react';
-// import Input from './input'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//     faFacebookSquare
-//   } from "@fortawesome/free-brands-svg-icons";
-
-// function Button(props:any) {
-
-//   return (
-//     <div>
-//        <button className="login-button" title="login" disabled={props.password? false:true}>Log In</button>
-//             <div className="separator">
-//                 <div className="line"></div>
-//                 <p>OR</p>
-//                 <div className="line"></div>
-//             </div>
-//             <div className="other">
-//                 <button className="fb-login-btn" type="button">
-//                     <FontAwesomeIcon icon={faFacebookSquare} size='1x'/>
-//                     <span className="otherlogin">Log in with Facebook</span>
-//                 </button>
-//                 <a className="forgot-password" href="#">Forgotten your password?</a>
-//             </div>
-//     </div>
-//   )
-// }
-
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faFacebookSquare
-  } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 
-interface Props{
-className:string;
-children?:React.ReactNode;
-onClick:()=>void;
-Password:boolean;
+interface Props {
+  className: string;
+  children?: React.ReactNode;
+  onClick: () => void;
+  Password: boolean;
 }
-const Button:React.FC<Props>=({ 
-  children,  
+const Button: React.FC<Props> = ({
+  children,
   onClick,
   className,
-  Password
-})=> {
+  Password,
+}) => {
   return (
     <div>
-            <button className={className} title="login" disabled={Password? false:true}>{children}</button>
-             <div className="separator">
-                 <div className="line"></div>
-                <p>OR</p>
-                <div className="line"></div>
-            </div>
-            <div className="other">
-                 <button className="fb-login-btn" type="button">
-                   <FontAwesomeIcon icon={faFacebookSquare} size='1x'/>
-                    <span className="otherlogin">Log in with Facebook</span>
-                 </button>
-                 <a className="forgot-password" href="#">Forgotten your password?</a>
-             </div>
+      <button
+        className={className}
+        title="login"
+        disabled={Password ? false : true}
+      >
+        {children}
+      </button>
+      <div className="separator">
+        <div className="line"></div>
+        <p>OR</p>
+        <div className="line"></div>
+      </div>
+      <div className="other">
+        <button className="fb-login-btn" type="button">
+          <FontAwesomeIcon icon={faFacebookSquare} size="1x" />
+          <span className="otherlogin">Log in with Facebook</span>
+        </button>
+        <a className="forgot-password" href="#">
+          Forgotten your password?
+        </a>
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Button;
