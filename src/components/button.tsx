51073c7a19ed4d0afe59a4a53eeb948f280b2558
Pulkit Anand <pulkitanand@Pulkit-Anands-MacBook-Pro.local>
2022-10-18 00:23:39 +0530

    error solved

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,7 +3,7 @@ import './Button.css'
 import {useDispatch} from 'react-redux';
 import {signInWithPopup} from "firebase/auth";
 import {auth,provider} from '../firebase/firebase';
-import {  setLogIn } from '../features/User/userSlice';
+import {  setLogIn } from '../reducers/userSlice';
 import { useNavigate } from "react-router-dom";
 interface Props{
 
